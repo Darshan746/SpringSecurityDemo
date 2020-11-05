a64155c4d0649ebb60d5db56dae70b0baf9bc1ef
Darshan <darshan.mn746@gmail.com>
2020-11-05 12:41:21 +0530

    trivial changes

---
+++ src/main/java/com/example/demo/auth/ApplicationUserService.java
@@ -0,0 +1,27 @@
+package com.example.demo.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserService implements UserDetailsService {
+
+
+    @Autowired
+    @Qualifier("fake")
+    private ApplicationUserDAO applicationUserDAO;
+
+    public ApplicationUserService(ApplicationUserDAO applicationUserDAO) {
+        this.applicationUserDAO = applicationUserDAO;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserDAO.selectApplicationUserByUserName(username)
+                .orElseThrow(()->  new UsernameNotFoundException("user Not Found"));
+    }
+}
---
+++ src/main/java/com/example/demo/auth/FakeApplicationDaoService.java
@@ -0,0 +1,54 @@
+package com.example.demo.auth;
+
+import com.example.demo.security.ApplicationUserRole;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository("fake")
+public class FakeApplicationDaoService implements  ApplicationUserDAO {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public FakeApplicationDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUserName(String userName) {
+        return getApplicationUser().stream().filter(applicationUser -> applicationUser.getUsername().equals(userName))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUser() {
+
+       return Lists.newArrayList(new ApplicationUser(ApplicationUserRole.STUDENT.getGrantedAuthorities()
+                        , passwordEncoder.encode("password")
+                        , "Rama"
+                        , true
+                        , true
+                        , true
+                        , true),
+                new ApplicationUser(ApplicationUserRole.ADMIN.getGrantedAuthorities()
+                        , passwordEncoder.encode("password")
+                        , "Krishna"
+                        , true
+                        , true
+                        , true
+                        , true)
+                , new ApplicationUser(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities()
+                        , passwordEncoder.encode("password")
+                        , "LINDA"
+                        , true
+                        , true
+                        , true
+                        , true));
+    }
+}
+
